607c7aafc7bf27472be1dbdaa43448422f2efd22
HalaMadi <Halamadi75@gmail.com>
2025-01-25 21:22:49 +0200

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AuthLayout from './layouts/AuthLayout'
+import Register from './pages/user/register/Register'
+import DashboardLayout from './layouts/DashboardLayout'
+import LogIn from './pages/user/login/LogIn'
 
 const App = () => {
-
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <AuthLayout />,
+      children: [
+        {
+          path: '/register',
+          element: <Register />
+        },
+        {
+          path: '/login',
+          element: <LogIn />
+        }
+      ]
+    },
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />
+    }
+  ])
   return (
-    <div>App</div>
+    <>
+     <RouterProvider router={router}/> 
+    </>
   )
 }
 
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/user/navbar/CustomNavbar'
+import { Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
   return (
-    <div>AuthLayout</div>
+    <div>
+      <Navbar/>
+      <Outlet/>
+    </div>
   )
 }
 
