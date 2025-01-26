9937de1bf05cba42c54bc804af6f633621af73d1
HalaMadi <Halamadi75@gmail.com>
2025-01-26 20:55:59 +0200

    Implement LogIn component with data submission

--- src/pages/user/login/LogIn.jsx
+++ src/pages/user/login/LogIn.jsx
@@ -1,15 +1,40 @@
 import axios from "axios";
+import { useState } from "react";
 import { FloatingLabel, Form, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { Bounce, toast } from "react-toastify";
 
 const LogIn = () => {
+  const [loader, setLoader] = useState(false);
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
   const handleLogIn = async (value) => {
-
+    setLoader(true);
+    try {
+      const response = await axios.post('https://ecommerce-node4.onrender.com/auth/signin', value)
+      if (response.status === 200) {
+        navigate('/')
+      }
+    } catch(error) {
+      toast.error(error.response.data.message, {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+        });
+    } finally {
+      setLoader(false);
+    }
   }
   return (
     <Form onSubmit={handleSubmit(handleLogIn)}>
@@ -37,8 +62,8 @@ const LogIn = () => {
         />
         {errors.password && (<div className="text-danger">{errors.password.message}</div>)}
       </FloatingLabel>
-      <Button type="submit" variant="primary">
-        Log In
+      <Button type="submit" variant="primary" disable={loader}>
+        {loader ? "Wait..." : "Log In"}
       </Button>
     </Form>
   );
